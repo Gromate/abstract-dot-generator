4c87e808e0e9a7aaf2fd2b7ee00abd38a44dd684
Mateusz Grochowski <groszek20027@gmail.com>
2023-06-24 17:45:23 +0200

    Added Dot class

---
+++ dot.js
@@ -0,0 +1,15 @@
+class Dot {
+    x;
+    y;
+    radius;
+    color;
+
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+}
+
+export default Dot;
