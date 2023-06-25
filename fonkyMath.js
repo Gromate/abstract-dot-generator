c0a92457513fbbe00d2728933657bcd3d4df9891
Mateusz Grochowski <groszek20027@gmail.com>
2023-06-25 11:31:42 +0200

    Moved math functions to separate module

---
+++ fonkyMath.js
@@ -0,0 +1,31 @@
+//get random Int smaller than max
+export function getRandomIntMax(max) {
+    return Math.floor(Math.random() * max);
+}
+
+//get random Int between min and max
+export function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+export function getRandomArbitraryMax(max) {
+    return Math.random() * max;
+}
+
+export function smoothing(number) {
+    if (number < 0.2) {
+        return number * 2;
+    }
+    else if (number > 0.8) {
+        return number * 2 - 1;
+    }
+    else {
+        return number / 2 + 0.25;
+    }
+}
+
+export function getRandomIntMaxSmoothing(max) {
+    return Math.floor(smoothing(Math.random()) * max);
+}
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { Dot } from "./dot.js";
+import * as fonkyMath from "./fonkyMath.js";
 
 var sliderDotCount = document.getElementById("dotCount");
 var outputDotCount = document.getElementById("outputDotCount");
@@ -8,45 +9,12 @@ var sliderDotScale = document.getElementById("dotScale");
 var outputDotScale = document.getElementById("outputDotScale");
 outputDotScale.innerHTML = sliderDotScale.value;
 
-//get random Int smaller than max
-function getRandomIntMax(max) {
-    return Math.floor(Math.random() * max);
-}
-
-//get random Int between min and max
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
-
-function getRandomArbitraryMax(max) {
-    return Math.random() * max;
-}
-
 function getRandomColor() {
     return `rgb(
-        ${getRandomArbitraryMax(255)},
-        ${getRandomArbitraryMax(255)},
-        ${getRandomArbitraryMax(255)}
+        ${fonkyMath.getRandomArbitraryMax(255)},
+        ${fonkyMath.getRandomArbitraryMax(255)},
+        ${fonkyMath.getRandomArbitraryMax(255)}
     )`
-}
-
-
-function smoothing(number) {
-    if (number < 0.2) {
-        return number * 2;
-    }
-    else if (number > 0.8) {
-        return number * 2 - 1;
-    }
-    else {
-        return number / 2 + 0.25;
-    }
-}
-
-function getRandomIntMaxSmoothing(max) {
-    return Math.floor(smoothing(Math.random()) * max);
 }
 
 CanvasRenderingContext2D.prototype.circle = function (x, y, radius) {
@@ -79,8 +47,8 @@ function draw() {
 
         while (dots.length <= dotCount) {
             dots.push(new Dot(
-                getRandomIntMax(ctx.canvas.width), //x
-                getRandomIntMax(ctx.canvas.height), //y
+                fonkyMath.getRandomIntMax(ctx.canvas.width), //x
+                fonkyMath.getRandomIntMax(ctx.canvas.height), //y
                 Math.random(), //radius
                 getRandomColorPalette(colorPalette) //color
             ));
